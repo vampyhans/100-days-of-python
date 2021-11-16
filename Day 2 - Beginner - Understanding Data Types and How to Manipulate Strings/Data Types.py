b75b3bf0d8ee82fb3f687e36cd069763b8d2dfb9
unknown <hansahimasarawijesinghe@gmail.com>
2021-11-17 00:03:53 +0530

    day 2 not complete

---
+++ Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/Data Types.py
@@ -0,0 +1,11 @@
+from typing import final
+
+
+marks = input("Input two digits number ")
+
+f_marks = marks[0]
+l_marks = marks[1]
+
+final = int(f_marks) + int(l_marks)
+
+print(int(final))
---
+++ Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/Type Error, Type Checking and Type Conversion.py
@@ -0,0 +1,13 @@
+#len in numbers
+
+num_char = len(input("What is your name ? "))
+
+new_num_char = str(num_char)
+
+print(new_num_char)
+
+#type check
+
+a =123
+print(type(a))
+
